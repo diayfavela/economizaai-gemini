dcb209f8b7b2a2b2f5d79912ed7b5e62f1702ffe
diayfavela <jailton.favela01@gmail.com>
2025-04-22 06:45:10 -0300

    Troca para gemini-1.5-flash

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-pro-vision")
+model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+
 
 app = Flask(__name__)
 
